2c6aecdcfa956ea1638eb531e598a5d5f83bea13
staminhe42 <staminhe42@gmail.com>
2019-05-25 23:33:56 +0200

    affichage du formulaire

--- d04/ex02/views.py
+++ d04/ex02/views.py
@@ -1,12 +1,13 @@
 from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import render
+from .forms import my_form
 
 
 
 def formulaire(request):
 	# construction du formulaire vide
-	form = formulaire(request.POST)
+	form = my_form(request.POST)
 	# si le formulaire est ok, recuperation des donnees
 	if form.is_valid():
 		text = form.cleaned_data['text']
